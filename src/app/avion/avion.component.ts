4db2e6931c28327a7d972c9edff41d339f7de4a4
b4sinnovation2019 <bs4innovation2019@gmail.com>
2019-12-20 22:03:02 +0000

    first committ

---
+++ src/app/avion/avion.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+selector: 'avion-component',
+templateUrl: './avion.component.html',
+styleUrls: ['./avion.component.css']
+})
+export class AvionComponent implements OnInit {
+
+constructor() {
+}
+
+ngOnInit(){
+
+}
+}
---
+++ src/app/mouvement/mouvement.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+selector: 'mouvement-component',
+templateUrl: './mouvement.component.html',
+styleUrls: ['./mouvement.component.css']
+})
+export class MouvementComponent implements OnInit {
+
+constructor() {
+}
+
+ngOnInit(){
+
+}
+}
